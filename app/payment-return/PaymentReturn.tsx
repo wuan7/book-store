9f34375229e8356aed13af5ab4c7d50378910cac
anhquantrieu <anhquantrieu.01@gmail.com>
2025-05-06 12:33:23 +0700

    update payment component

--- app/payment-return/PaymentReturn.tsx
+++ app/payment-return/PaymentReturn.tsx
@@ -2,12 +2,9 @@
 import { useEffect, useState } from "react";
 import confetti from "canvas-confetti";
 import { useSearchParams, useRouter } from "next/navigation";
-import { CheckCircle, Loader, XCircle } from "lucide-react";
+import { CheckCircle,  Loader2, XCircle } from "lucide-react";
 import { getOrderById } from "../.././actions/order";
-import {
-  OrderItemWithProduct,
-  OrderWithOrderItemsAndProduct,
-} from "../.././types";
+import { OrderWithOrderItemsAndProduct } from "../.././types";
 const PaymentReturn = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -69,82 +66,28 @@ const PaymentReturn = () => {
   }, [success]);
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
-      {success === null ? (
-        <p>Đang kiểm tra trạng thái thanh toán...</p>
-      ) : success && order ? (
+      {success === null ||
+        (isLoading && (
+          <div className="flex flex-col items-center max-w-3xl h-28">
+            <Loader2 className="animate-spin size-6" />
+          </div>
+        ))}
+
+      {success === false && (
+        <div className="flex flex-col items-center">
+          <XCircle size={64} className="text-red-600" />
+          <h1 className="text-2xl font-bold mt-4 text-red-600">
+            Thanh toán thất bại
+          </h1>
+        </div>
+      )}
+
+      {success && order && (
         <div className="flex flex-col items-center">
           <CheckCircle size={64} className="text-green-600" />
           <h1 className="text-2xl font-bold mt-4 text-green-600">
             Thanh toán thành công!
           </h1>
-          {order ? (
-            <div className="my-3 shadow-2xl p-5 space-y-1.5">
-              <div className="flex items-center justify-between gap-x-2">
-                <p className="font-bold text-primary">Mã thanh toán:</p>
-                <p className="text-sm font-semibold">{order.id}</p>
-              </div>
-              <div className="flex items-center justify-between gap-x-2">
-                <p className="font-bold text-primary">Trạng thái:</p>
-                <p className="text-sm font-semibold">
-                  {order.status === "PAID"
-                    ? "Đã thanh toán"
-                    : "Chưa thanh toán"}
-                </p>
-              </div>
-              {order.orderItems.map((item: OrderItemWithProduct) => (
-                <>
-                  <div className="flex items-center justify-between gap-x-2">
-                    <p className="font-bold text-primary">Sách:</p>
-                    <p className="text-sm font-semibold">{item.product.name}</p>
-                  </div>
-                  <div className="flex items-center justify-between gap-x-2">
-                    <p className="font-bold text-primary">Giá:</p>
-                    <p className="text-sm font-semibold">
-                      {" "}
-                      {item.price.toLocaleString("vi-VN", {
-                        style: "currency",
-                        currency: "VND",
-                      })}
-                    </p>
-                  </div>
-
-                  <div className="flex items-center justify-between gap-x-2">
-                    <p className="font-bold text-primary">Số lượng:</p>
-                    <p className="text-sm font-semibold">
-                      {item.quantity} cuốn
-                    </p>
-                  </div>
-                </>
-              ))}
-
-              <div className="flex items-center justify-between gap-x-2">
-                <p className="font-bold text-primary">Thời gian thanh toán:</p>
-                <p className="text-sm font-semibold">
-                  {new Date(order.createdAt).toLocaleString("vi-VN")}
-                </p>
-              </div>
-            </div>
-          ) : (
-            <div>
-              <h1 className="font-bold text-red-600">
-                Không tìm thấy thông tin tour.
-              </h1>
-            </div>
-          )}
-
-          {isLoading && (
-            <div className="mt-4 p-10">
-              <Loader className="size-6 animate-spin text-primary" />
-            </div>
-          )}
-        </div>
-      ) : (
-        <div className="text-red-600 flex items-center flex-col">
-          <XCircle size={64} />
-          <h1 className="text-2xl font-bold mt-4">
-            {!order ? "Có lỗi xảy ra" : "Thanh toán không thành công"}
-          </h1>
-          <p>Vui lòng thử lại hoặc liên hệ hỗ trợ.</p>
         </div>
       )}
     </div>
